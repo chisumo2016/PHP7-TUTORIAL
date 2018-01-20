c1c314b96c541c1a5975aa02e9b62b409e19126d
= <=>
2018-01-20 23:29:26 +0000

    Spaceship operator

---
+++ spaceship_operator.php
@@ -0,0 +1,116 @@
+<?php
+
+//SpaceShip Operator
+
+//-1 less than
+// o equal to      2<=> 2
+
+// 1 greater than
+
+/*$spaceshipOperator = 1 <=> 2;  // -1 greater than  means the value of the left is less than
+
+$spaceshipOperator = 2<=> 2; // 0 equal to   means the value of the left is equal to
+
+$spaceshipOperator = 200<=> 2;  // 1 greater than  means the value of the left is greater than
+
+$spaceshipOperator = 200<=> "2";  // Poly mormophysim
+
+$spaceshipOperator = 200<=> "2.22";  //
+
+$spaceshipOperator = 2<=> true ;// Poly mormophysi
+
+$spaceshipOperator = "2"<=> true ;// Poly mormophysi
+$spaceshipOperator = array( 20, 20, 20 )<=> array( 20, 20, 20 ) ;// Poly mormophysi
+
+echo  $spaceshipOperator; */
+
+/*
+if  (  array( 20, 20, 20  ) <=> array( 20, 20, 20 ) ) {
+
+    echo "Something happened" ;
+
+};
+*/
+
+/*
+if  (  (array( 200, 20, 20  ) <=> array( 20, 20, 20 ) ) == 1 ){
+
+echo "Greater than" ;
+
+}; */
+
+ $a =  array( 200, 20, 20  ) <=> array( 20, 20, 20 );
+if  ( (  $a ) == 1 ){
+
+    echo "Greater than" ;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
