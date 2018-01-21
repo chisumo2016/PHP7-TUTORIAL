43e24f2184bd445675c35563df3c3c5613363234
= <=>
2018-01-21 00:04:36 +0000

    Uncode codepoint escape syntax

---
+++ unicode_codepoint_escape_syntax.php
@@ -0,0 +1,56 @@
+<?php
+// Many diffrent language around the world
+
+//echo "&pound;";
+//echo "&emp;";
+
+
+//PHP 7  check in ws3school  UTF 8
+
+echo "\u{2200}";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
